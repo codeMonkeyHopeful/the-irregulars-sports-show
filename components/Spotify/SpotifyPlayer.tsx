58c77c9ed306a83feb929066150861b6d8fb0798
Ryan Jasinski <jasinski.17@gmail.com>
2026-05-09 15:26:48 -0700

    formatting

--- components/Spotify/SpotifyPlayer.tsx
+++ components/Spotify/SpotifyPlayer.tsx
@@ -1,12 +1,14 @@
 const SpotifyPlayer = () => {
   return (
-    <iframe
-      src="https://open.spotify.com/embed/show/0YdcBxpNuJzRcQ8JddLsc9"
-      width="100%"
-      height="352"
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      loading="lazy"
-    />
+    <div className="spotify-player">
+      <iframe
+        src="https://open.spotify.com/embed/show/0YdcBxpNuJzRcQ8JddLsc9"
+        width="100%"
+        height="352"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
+    </div>
   );
 };
 
