89b201abe4191bc32c88404ab66c42367082caaa
codeMonkeyHopeful <jasinski.17@gmail.com>
2026-06-18 19:51:34 -0700

    all pages proper

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,18 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'On the Field',
+  title: 'Contact Us',
   description: 'A podcast about sports.',
 };
 
-export default function OnTheField() {
+export default function ContactUs() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          <span className="title">On the Field</span>
+          <span className="title">Contact Us</span>
         </h1>
-        <div>Photos go here</div>
+        <div>Contact information goes here</div>
       </main>
     </div>
   );
