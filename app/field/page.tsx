000c321d90569233fa040a18cbdc54aeea48d558
codeMonkeyHopeful <jasinski.17@gmail.com>
2026-06-18 19:47:50 -0700

    forgot to commit

---
+++ app/field/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'In the Field',
+  description: 'A podcast about sports.',
+};
+
+export default function About() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <span className="title">In the Wild</span>
+        </h1>
+        <div>Photos go here</div>
+      </main>
+    </div>
+  );
+}
--- constants/navItems.tsx
+++ constants/navItems.tsx
@@ -1,4 +1,4 @@
-import { Home, Info, Music2, Play } from 'lucide-react';
+import { Home, Image, Music2, Play, Send, Users } from 'lucide-react';
 
 export const navItems = [
   { label: 'Home', icon: Home, href: '/' },
@@ -8,5 +8,7 @@ export const navItems = [
     icon: Music2,
     href: 'https://open.spotify.com/show/6EwXjHiWsRiE9SE8GuJJv4',
   },
-  { label: 'The Team', icon: Info, href: '/about' },
+  { label: 'The Team', icon: Users, href: '/team' },
+  { label: 'On the Field', icon: Image, href: '/field' },
+  { label: 'Contact Us', icon: Send, href: '/contact' },
 ];
