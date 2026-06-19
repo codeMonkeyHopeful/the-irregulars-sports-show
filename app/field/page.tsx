e5985b579d350869e772e6de931e6ddd3b55272d
codeMonkeyHopeful <jasinski.17@gmail.com>
2026-06-18 19:48:27 -0700

    update names

--- app/field/page.tsx
+++ app/field/page.tsx
@@ -5,12 +5,12 @@ export const metadata: Metadata = {
   description: 'A podcast about sports.',
 };
 
-export default function About() {
+export default function InTheField() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          <span className="title">In the Wild</span>
+          <span className="title">In the Field</span>
         </h1>
         <div>Photos go here</div>
       </main>
