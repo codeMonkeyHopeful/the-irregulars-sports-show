943f6b300dfcb870619286c9a82d23bc7da79bea
codeMonkeyHopeful <jasinski.17@gmail.com>
2026-06-18 20:46:42 -0700

    perfect

--- app/field/page.tsx
+++ app/field/page.tsx
@@ -1,13 +1,27 @@
+import fs from 'fs';
 import { Metadata } from 'next';
+import path from 'path';
 import PhotoGallery from './PhotoGallery';
-import { photos } from './photos';
 
 export const metadata: Metadata = {
   title: 'On the Field',
   description: 'A podcast about sports.',
 };
 
+function getPhotos() {
+  const dir = path.join(process.cwd(), 'public/photos');
+
+  return fs
+    .readdirSync(dir)
+    .filter((file) => /\.(jpg|jpeg|png|webp)$/i.test(file))
+    .sort()
+    .map((file) => ({
+      id: file,
+      src: `/photos/${file}`,
+    }));
+}
 export default function OnTheField() {
+  const photos = getPhotos();
   return (
     <div className="font-sans min-h-screen p-8 sm:p-20">
       <main className="flex flex-col items-center gap-10">
